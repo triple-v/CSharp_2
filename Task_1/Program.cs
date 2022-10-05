40039568ef0dcd16572796c70b2f569d4aadf3a9
Vladislav <vasilevich.account@yandex.ru>
2022-10-06 00:07:38 +0300

    Решена задача 10

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите трёхзначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (99 < num && num < 1000) {
+int secondDigit = num % 100 / 10;
+Console.WriteLine($"{secondDigit}");
+} else {
+  Console.WriteLine("Число не является трёхзначным.");
+}

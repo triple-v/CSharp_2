406a9014338626a2b009f4819285504de8fba049
Vladislav <vasilevich.account@yandex.ru>
2022-10-06 00:58:26 +0300

    Решена задача 13

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+while (999 < number){
+  number = number / 10;
+}
+if (99 < number) {
+Console.WriteLine(number % 10);
+} else {
+  Console.WriteLine("Третьей цифры нет.");
+}

5d3fa4ea02797913b832c86e0845668d10e91143
Vladislav <vasilevich.account@yandex.ru>
2022-10-07 19:51:10 +0300

    Решена задача 15

---
+++ Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1 до 7:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Число вне допустимого диапазона.");
+} else if (number >= 1 && number <= 5) {
+    Console.WriteLine("Не выходной.");
+} else {
+    Console.WriteLine("Выходной.");
+}
